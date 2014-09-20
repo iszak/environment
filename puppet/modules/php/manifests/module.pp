475684a49b973870c39175b5f9ffdb3006a623e8
Iszak Bryan <iszak.bryan@gmail.com>
2014-09-20 22:23:51 +0100

    Added basic php module class

--- puppet/modules/php/manifests/module.pp
+++ puppet/modules/php/manifests/module.pp
@@ -8,6 +8,17 @@
 #
 #  php::module { 'gd': }
 #
-define php::module () {
+define php::module (
+  $package_prefix = 'php5-'
+) {
   include php::params
+
+  $package_prefix_param = $package_prefix ? {
+    undef   => $::php::params::package_prefix,
+    default => $package_prefix,
+  }
+
+  package { "${package_prefix_param}-${name}":
+    ensure => latest
+  }
 }
