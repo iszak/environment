bc9477d55647fbc8fe118586274c3ebe57dbefba
Iszak Bryan <iszak.bryan@gmail.com>
2014-09-21 00:00:20 +0100

    Added todo, added class to install gems

---
+++ puppet/modules/ruby/manifests/gem.pp
@@ -0,0 +1,18 @@
+# == Define: ruby::gem
+#
+# A type to install ruby gems
+#
+# === Parameters
+#
+# === Examples
+#
+#  ruby::gem { 'bundler': }
+#
+define ruby::gem (
+
+) {
+  package { $name:
+    ensure   => installed,
+    provider => 'gem',
+  }
+}
