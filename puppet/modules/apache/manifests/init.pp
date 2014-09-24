d2833a73930dfcefc598c557bb8694ae71e47043
Iszak Bryan <iszak.bryan@gmail.com>
2014-09-25 00:24:55 +0100

    Register apache2 as a service

--- puppet/modules/apache/manifests/init.pp
+++ puppet/modules/apache/manifests/init.pp
@@ -15,4 +15,10 @@ class apache () {
   package { 'apache2':
     ensure => latest
   }
+
+  service { 'apache2':
+    ensure  => running,
+    enable  => true,
+    require => Package['apache2'],
+  }
 }
