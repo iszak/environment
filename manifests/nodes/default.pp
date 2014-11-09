0f6a089662b24e00e3fcfb43c956274b79057f40
Iszak Bryan <iszak.bryan@gmail.com>
2014-11-09 15:35:10 +0000

    Use hiera for config/keys

--- manifests/nodes/default.pp
+++ manifests/nodes/default.pp
@@ -4,8 +4,10 @@ node default {
   create_resources(user, hiera_hash('user', {}))
   create_resources(file, hiera_hash('file', {}))
 
+  create_resources('ssh::key', hiera_hash('ssh::key', {}))
   create_resources('ssh::known_host', hiera_hash('ssh::known_host', {}))
   create_resources('ssh::authorized_key', hiera_hash('ssh::authorized_key', {}))
+  create_resources('ssh::config', hiera_hash('ssh::config', {}))
 
   create_resources('zsh::config', hiera_hash('zsh::config', {}))
   create_resources('sudoers::config', hiera_hash('sudoers::config', {}))
