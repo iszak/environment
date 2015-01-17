a61864164817cf7640742dab07e00c855968bbb3
Iszak Bryan <iszak.bryan@gmail.com>
2015-01-17 22:21:00 +0000

    Automating creationg of super user config

--- README.md
+++ README.md
@@ -2,6 +2,23 @@
 
 ## Installation
 
+### Vagrant
+
+- Install latest [Vagrant](https://www.vagrantup.com/)
+- Install [vagrant-sshfs](https://github.com/fabiokr/vagrant-sshfs) plugin ```vagrant plugin install vagrant-sshfs```
+
+### VirtualBox
+
+- Install latest [VirtualBox](https://www.virtualbox.org/)
+
+
+## Configuration
+
+Run ```./setup.sh```
+
+## Packer
+
+Build the base vagrant box
 
 ```
 cd packer/
@@ -11,9 +28,9 @@ vagrant up
 ```
 
 
-## Crowdwish
+## Vagrant
 
-- Edit vagrant/puppet/hiera.yaml and add projects you want, example below
+Edit vagrant/puppet/hiera.yaml and add projects you want, example below
 
 ```
 ---
@@ -43,24 +60,21 @@ vagrant up
 ```
 
 
-- Create **vagrant/puppet/hiera/private.yaml** example below
+Create **vagrant/puppet/hiera/private.yaml** example below
 
 ```
 ---
 file:
-    crowdwish_ssh_gitlab:
-        ensure: present
-        require:
-            - User[crowdwish]
-            - File[crowdwish_ssh]
-        path: /home/crowdwish/.ssh/gitlab.key
-        content: "CONTENT HERE"
-        owner: crowdwish
-        group: crowdwish
-        mode: 0600
+    vagrant_ssh_default:
+        content: "PERSONAL KEY"
+
+project::static:
+    crowdwish_client:
+        ssh_private_key: "PRIVATE KEY"
+        ssh_public_key: "PUBLIC KEY"
 ```
 
-- Edit the "content" key so the value is your SSH key with new lines replaced with literal "\n" a command below will format your key and copy it to your clipboard
+Change the personal key with the key you will be pushing/pulling with and the they key for the project, this may be the same key. Replace all the new lines with "\n" string literal, you can copy it with the command below
 
 ```
 cat ~/.ssh/id_rsa | perl -p -e 's/\n/\\n/' | pbcopy
--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env sh
 
+PACKER_VARIABLES="packer/variables.json"
+UBUNTU_PRESEED="packer/config/preseed.cfg"
+VAGRANT_FILE="vagrant/Vagrantfile"
+HIERA_USER="vagrant/provisioners/puppet/hiera/users/user.yaml"
+
 echo "Enter a full name: "
 read full_name
 
@@ -12,6 +17,13 @@ read username
 
 if [ -z "$username" ]; then
     username="vagrant"
+fi
+
+echo "Enter a email: "
+read email
+
+if [ -z "$email" ]; then
+    email="email@address.com"
 fi
 
 echo "Enter a password: "
@@ -28,24 +40,48 @@ if [ -z "$hostname" ]; then
     hostname="vagrant"
 fi
 
+
 # TODO: Use key over password
 #ssh-keygen -f file.rsa -t rsa -N ''
 
 # Copy templates
-cp packer/variables.json.template packer/variables.json
-cp packer/config/preseed.cfg.template packer/config/preseed.cfg
+# if [ ! -f "$PACKER_VARIABLES" ]; then
+    cp "$PACKER_VARIABLES.template" "$PACKER_VARIABLES"
+# fi
 
-cp vagrant/Vagrantfile.template vagrant/Vagrantfile
+# if [ ! -f "$UBUNTU_PRESEED" ]; then
+    cp "$UBUNTU_PRESEED.template" "$UBUNTU_PRESEED"
+# fi
 
-sed -i '' "s/FULLNAME/iszak/g" packer/config/preseed.cfg
-sed -i '' "s/USERNAME/$username/g" packer/config/preseed.cfg
-sed -i '' "s/PASSWORD/$password/g" packer/config/preseed.cfg
+# if [ ! -f "$VAGRANT_FILE" ]; then
+    cp "$VAGRANT_FILE.template" "$VAGRANT_FILE"
+# fi
 
-sed -i '' "s/USERNAME/$username/g" packer/variables.json
-sed -i '' "s/PASSWORD/$password/g" packer/variables.json
-sed -i '' "s/HOSTNAME/$hostname/g" packer/variables.json
+# if [ ! -f "$HIERA_USER" ]; then
+    cp "$HIERA_USER.template" "$HIERA_USER"
+# fi
 
-sed -i '' "s/USERNAME/$username/g" vagrant/Vagrantfile
-sed -i '' "s/PASSWORD/$password/g" vagrant/Vagrantfile
-sed -i '' "s/HOSTNAME/$hostname/g" vagrant/Vagrantfile
+# TODO: Fix
+password_hash=$(echo -n "$password$username" | md5)
 
+# Ubuntu
+sed -i '' "s/FULLNAME/$full_name/g" "$UBUNTU_PRESEED"
+sed -i '' "s/USERNAME/$username/g" "$UBUNTU_PRESEED"
+sed -i '' "s/PASSWORD/$password/g" "$UBUNTU_PRESEED"
+
+# Packer
+sed -i '' "s/USERNAME/$username/g" "$PACKER_VARIABLES"
+sed -i '' "s/PASSWORD/$password/g" "$PACKER_VARIABLES"
+sed -i '' "s/HOSTNAME/$hostname/g" "$PACKER_VARIABLES"
+
+# Vagrant
+sed -i '' "s/USERNAME/$username/g" "$VAGRANT_FILE"
+sed -i '' "s/PASSWORD/$password/g" "$VAGRANT_FILE"
+sed -i '' "s/HOSTNAME/$hostname/g" "$VAGRANT_FILE"
+
+
+# Hiera
+sed -i '' "s/USERNAME/$username/g" "$HIERA_USER"
+sed -i '' "s/FULLNAME/$full_name/g" "$HIERA_USER"
+sed -i '' "s/EMAIL/$email/g" "$HIERA_USER"
+sed -i '' "s/PASSWORD_HASH/$password_hash/g" "$HIERA_USER"
