2be91065814150e6bd2149454b07921aa4e6c758
Iszak Bryan <iszak.bryan@gmail.com>
2015-08-02 19:30:05 +0100

    Use new variable names, added git user name support

--- setup.sh
+++ setup.sh
@@ -1,28 +1,28 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 PACKER_VARIABLES="packer/variables/vagrant.json"
 UBUNTU_PRESEED="packer/config/preseed.cfg"
 VAGRANT_FILE="vagrant/Vagrantfile"
-HIERA_USER="vagrant/provisioners/puppet/hiera/users/user.yaml"
+HIERA_USER="vagrant/provisioners/puppet/environments/development/hieradata/users/user.yaml"
 
 
 
 echo "Enter a full name for the OS (Vagrant): "
-read os_full_name
+read -r os_full_name
 
 if [ -z "$os_full_name" ]; then
     os_full_name="Vagrant"
 fi
 
 echo "Enter a username for the OS (vagrant): "
-read os_username
+read -r os_username
 
 if [ -z "$os_username" ]; then
     os_username="vagrant"
 fi
 
 echo "Enter a hostname for the OS (vagrant): "
-read os_hostname
+read -r os_hostname
 
 if [ -z "$os_hostname" ]; then
     os_hostname="vagrant"
@@ -30,25 +30,35 @@ fi
 
 
 echo "Enter a password for the OS (vagrant): "
-read os_password
+read -r os_password
 
 if [ -z "$os_password" ]; then
     os_password="vagrant"
 fi
 
 
-echo "Enter a email for Git (email@address.com): "
-read git_email
+echo "Enter a email for Git: "
+read -r git_email
 
 if [ -z "$git_email" ]; then
-    git_email="email@address.com"
+    echo "You must provide a email for Git" 1>&2
+    exit 1
 fi
 
-echo "Enter a full name for Git (Vagrant): "
-read git_full_name
+echo "Enter a full name for Git: "
+read -r git_name
 
-if [ -z "$git_full_name" ]; then
-    git_full_name="Vagrant"
+if [ -z "$git_name" ]; then
+    echo "You must provide a name for Git" 1>&2
+    exit 1
+fi
+
+echo "Enter a user name for Github: "
+read -r git_user
+
+if [ -z "$git_user" ]; then
+    echo "You must provide a user name for Github" 1>&2
+    exit 1
 fi
 
 
@@ -62,7 +72,8 @@ cp "$VAGRANT_FILE.template" "$VAGRANT_FILE"
 cp "$HIERA_USER.template" "$HIERA_USER"
 
 # TODO: Fix
-password_hash=$(echo -n "$os_password$os_username" | md5)
+postgres_user=$os_username
+postgres_password=$(echo -n "$os_password$os_username" | md5)
 
 # Ubuntu
 sed -i '' "s/FULLNAME/$os_full_name/g" "$UBUNTU_PRESEED"
@@ -81,7 +92,11 @@ sed -i '' "s/HOSTNAME/$os_hostname/g" "$VAGRANT_FILE"
 
 
 # Hiera
+sed -i '' "s/POSTGRESQL_PASSWORD/$postgres_password/g" "$HIERA_USER"
+sed -i '' "s/POSTGRESQL_USER/$postgres_user/g" "$HIERA_USER"
+
 sed -i '' "s/USERNAME/$os_username/g" "$HIERA_USER"
-sed -i '' "s/FULLNAME/$git_full_name/g" "$HIERA_USER"
-sed -i '' "s/EMAIL/$git_email/g" "$HIERA_USER"
-sed -i '' "s/PASSWORD_HASH/$password_hash/g" "$HIERA_USER"
+sed -i '' "s/PASSWORD/$os_password/g" "$HIERA_USER"
+sed -i '' "s/GIT_NAME/$git_name/g" "$HIERA_USER"
+sed -i '' "s/GIT_EMAIL/$git_email/g" "$HIERA_USER"
+sed -i '' "s/GIT_USER/$git_user/g" "$HIERA_USER"
